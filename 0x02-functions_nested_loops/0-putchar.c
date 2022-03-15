7708469fca0f121e477f63866eb2d0f3a86dad90
Lawizzy01 <aremulawal989@gmail.com>
2022-03-15 09:47:43 +0000

    Print AlxAfrica using _putchar()

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "AlxAfrica.h"
+
+/**
+ * main -  prints AlxAfrica, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+       	_putchar('A');
+ 	_putchar('l');
+	_putchar('x');
+	_putchar('A');
+	_putchar('f');
+	_putchar('r');
+       	_putchar('i');
+	_putchar('c');
+	_putchar('a');
+	_putchar('\n');
+	return (0);
+}
